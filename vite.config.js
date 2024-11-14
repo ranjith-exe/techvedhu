aef104dd011745f8da190a7603f01ff2ae45644a
ranjith-exe <ranjithmercer@gmail.com>
2024-11-14 15:44:54 +0530

    gh page deployement

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import pages from 'vite-plugin-react-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pages()],
   base: '/techvedhu/',
 })
