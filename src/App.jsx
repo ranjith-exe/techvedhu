dd74f92a3a3d33f7e5924d250caf34340b997c67
ranjith-exe <ranjithmercer@gmail.com>
2024-11-14 14:54:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+// Import the Header, Footer, and pages
+import Header from './include/theheader';
+import Footer from './include/thefooter';
+import Home from './pages/home'
+import About from './pages/about';
+import Contact from './pages/contact';
+import NotFound from './pages/notfound';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Header will be displayed on all pages */}
+        <Header />
+
+        <main>
+          <Routes>
+            {/* Define routes for your application */}
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} /> */}
+            <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
+          </Routes>
+        </main>
+
+        {/* Footer will be displayed on all pages */}
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
