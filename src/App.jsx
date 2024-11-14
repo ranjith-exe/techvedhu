b90ad5be5d2c172c8c02e53c3c0d2bb5fdff86fd
ranjith-exe <ranjithmercer@gmail.com>
2024-11-14 16:31:52 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-// App.jsx
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 // Import the Header, Footer, and pages
 import Header from './include/theheader';
 import Footer from './include/thefooter';
-import Home from './pages/home'
+import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import NotFound from './pages/notfound';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/techvedhu"> {/* Specify the base path for GitHub Pages */}
       <div>
         {/* Header will be displayed on all pages */}
         <Header />
@@ -22,10 +19,10 @@ function App() {
         <main>
           <Routes>
             {/* Define routes for your application */}
-            <Route path="/techvedhu/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} /> Catch-all route for 404
+            <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
           </Routes>
         </main>
 
